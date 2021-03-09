71d1f590a73a09885cd9e7c0dc91fa0981e6ec62
LuongVuXuan <36665799+LuongDzai@users.noreply.github.com>
2021-03-09 23:52:24 +0700

    Add command line to run and code run many times in a moment

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const BASE_URL = "https://hoidap.thuvienphapluat.vn/tim-theo-tu-van.html";
 
 // Easy to Divide to run many times
-const START_INDEX = 1;
-const END_INDEX = 10; // max INDEX of this page is 12383
+let START_INDEX = 1;
+let END_INDEX = 100; // max INDEX of this page is 12383
+let THREAD = 10;
+let MAX_TIMEOUT = 100; // milliseconds
 
-// Init records to write
-const records = [];
+// Get params from command line
+process.argv.forEach(function (val, index, array) {
+  if (index == 2) START_INDEX = parseInt(val);
+  if (index == 3) END_INDEX = parseInt(val);
+  if (index == 4) THREAD = parseInt(val);
+});
+
 // Init CSV name, path and columns of Writer
 const csvWriter = createCsvWriter({
   path: "file.csv",
@@ -18,39 +25,61 @@ const csvWriter = createCsvWriter({
   ],
 });
 
-async function main() {
+async function main(start, end, thread = 1) {
   // Init browser to crawl
   const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
 
   // For loop all page of link base
-  for (let index = START_INDEX; index <= END_INDEX; index++) {
-    console.log(`-----------Page ${index}----------`);
+  for (let index = start; index <= end; index++) {
     await page.goto(BASE_URL + `?page=${index}`);
     const urls = await page.$$eval("a.tieu-de", (elements) =>
       elements.map((el) => el.href)
     );
-    for (let index = 0; index < urls.length; index++) {
+    for (let i = 0; i < urls.length; i++) {
       try {
-        const url = urls[index];
+        const url = urls[i];
         const pageCrawl = await browser.newPage();
         await pageCrawl.goto(url);
-        const title = await pageCrawl.innerText(".fix-right h1");
-        const question = await pageCrawl.innerText(
-          ".fix-right .alert-warning p"
-        );
-        const answer = await pageCrawl.innerText("#cautraloi");
-        const record = [{ title, question, answer }];
-        await csvWriter.writeRecords(record);
-        console.log(`Done link ${index}`);
+
+        const title = await pageCrawl.innerText(".fix-right h1", {
+          timeout: MAX_TIMEOUT,
+        });
+        if (title) {
+          const question = await pageCrawl.innerText(
+            ".fix-right .alert-warning p"
+          );
+          const answer = await pageCrawl.innerText("#cautraloi");
+          const record = [{ title, question, answer }];
+          await csvWriter.writeRecords(record);
+        }
+        console.log(`Done link ${i + 1} - page ${index} - thread ${thread}`);
         await pageCrawl.close();
       } catch (error) {
         continue;
       }
     }
   }
-  console.log("Crawl Done!");
+  console.log(`^-^ Crawl Thread ${thread} Done!`);
   await browser.close();
 }
 
-main();
+function doCrawl(start, end, thread) {
+  const jump = Math.floor((end - start + 1) / thread);
+  for (let index = 0; index < thread; index++) {
+    main(
+      start + (jump - 1) * index + index,
+      start + (jump - 1) * (index + 1) + index,
+      index + 1
+    );
+  }
+}
+
+doCrawl(START_INDEX, END_INDEX, THREAD);
+/*
+  VD: 1 - 100 - 10
+  i runs from 0 - 9
+  1-10: 1 + i * ()
+  11-20
+  ...
+ */
